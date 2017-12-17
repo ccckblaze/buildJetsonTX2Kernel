5e307d66e9601f240d4a1f5a6e5e8d6aa75b9c95
Richard Everts <lesreaper@users.noreply.github.com>
2017-12-16 20:57:12 -0500

    Update fixMakeFiles.sh

--- scripts/fixMakeFiles.sh
+++ scripts/fixMakeFiles.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Fix the Makefiles so that they compile on the device
+echo "Patching devfreq"
 patch /usr/src/kernel/kernel-4.4/drivers/devfreq/Makefile ./diffs/devfreq/devfreq.patch
+echo "Patching nvgpu"
 patch /usr/src/kernel/nvgpu/drivers/gpu/nvgpu/Makefile ./diffs/nvgpu/nvgpu.patch
+echo "Patching tegra-alt"
+patch /usr/src/kernel/kernel-4.4/sound/soc/tegra-alt/Makefile ./diffs/tegra-alt/tegra-alt.patch
+echo "Patching tegra-alt"
 patch /usr/src/kernel/kernel-4.4/sound/soc/tegra-alt/Makefile ./diffs/tegra-alt/tegra-alt.patch
 # vmipi is in a sub directory without a Makefile, there was an include problem
 cp /usr/src/kernel/kernel-4.4/drivers/media/platform/tegra/mipical/mipi_cal.h /usr/src/kernel/kernel-4.4/drivers/media/platform/tegra/mipical/vmipi/mipi_cal.h
