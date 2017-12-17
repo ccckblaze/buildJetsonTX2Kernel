b19d3ef05ee95606d620fb70cd5de22325d58f9c
Richard Everts <lesreaper@users.noreply.github.com>
2017-12-17 12:06:18 -0500

    Update fixMakeFiles.sh

--- scripts/fixMakeFiles.sh
+++ scripts/fixMakeFiles.sh
@@ -6,12 +6,11 @@ echo "Patching nvgpu kernel..."
 patch /usr/src/kernel/nvgpu/drivers/gpu/nvgpu/Makefile ./diffs/nvgpu/nvgpu.patch
 echo "Patching tegra-alt kernel..."
 patch /usr/src/kernel/kernel-4.4/sound/soc/tegra-alt/Makefile ./diffs/tegra-alt/tegra-alt.patch
-echo "Patching uvc_driver.c driver file..."
-patch /usr/src/kernel/kernel-4.4/drivers/media/usb/uvc/uvc_driver.c ./diffs/uvc_driver_c.patch
-echo "Patching uvcvideo.h driver file..."
-patch /usr/src/kernel/kernel-4.4/drivers/media/usb/uvc/uvcvideo.h ./diffs/uvcvideo_h.patch
-echo "Patching videodev2.h driver file..."
-patch /usr/src/kernel/kernel-4.4/include/uapi/linux/videodev2.h ./diffs/videodev2_h.patch
+
+# Apply UVC formats patch for RealSense devices
+echo "Patching RealSense camera formats..."
+patch -p1 < ./diffs/realsense-camera-formats.patch
+
 # vmipi is in a sub directory without a Makefile, there was an include problem
 cp /usr/src/kernel/kernel-4.4/drivers/media/platform/tegra/mipical/mipi_cal.h /usr/src/kernel/kernel-4.4/drivers/media/platform/tegra/mipical/vmipi/mipi_cal.h
  
