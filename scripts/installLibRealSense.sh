56ddef721aea72bcd286a14c562cf795db92cba9
Richard Everts <lesreaper@users.noreply.github.com>
2017-12-16 21:22:10 -0500

    Create installLibRealSense.sh

---
+++ scripts/installLibRealSense.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Installs RealSense Cameras
+sudo apt-get install --install-recommends linux-generic-lts-xenial xserver-xorg-core-lts-xenial xserver-xorg-lts-xenial xserver-xorg-video-all-lts-xenial xserver-xorg-input-all-lts-xenial libwayland-egl1-mesa-lts-xenial
+sudo apt-get install libusb-1.0-0-dev pkg-config libgtk-3-dev
+sudo apt-get install libglfw3-dev
