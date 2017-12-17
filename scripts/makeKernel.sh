7f405369d297b6b9fdf52c8f77325c473a82c915
Richard Everts <lesreaper@users.noreply.github.com>
2017-12-17 12:24:14 -0500

    Update makeKernel.sh

--- scripts/makeKernel.sh
+++ scripts/makeKernel.sh
@@ -4,6 +4,11 @@
 cd /usr/src/kernel/kernel-4.4
 make prepare
 make modules_prepare
+
+# Build SPI module
+sudo make M=drivers/spi
+sudo cp /usr/src/kernel/kernel-4.4/drivers/spi/spidev.ko /lib/modules/$(uname -r)/kernel/drivers/
+
 # Make alone will build the dts files too
 # make -j6
 make -j6 Image
